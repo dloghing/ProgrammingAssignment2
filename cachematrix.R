9625bf11c07ca56d228169ba81313012a3195fd7
David Loghing <dlog@email.com>
2015-10-19 15:02:06 -0400

    Tested with invertible matrices (1,-1,1,2) and (2,-2,2,4)
    Signed-off-by: David Loghing <dlog@email.com>

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,26 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+    matrix_inverse <- NULL
+    set <- function(y) {
+        x <<- y
+        matrix_inverse <<- NULL
+    }
+    
+    get <- function() {
+        x
+    }
+    
+    setinverse <- function(solve) {
+        matrix_inverse <<- solve
+    }
+
+    getinverse <- function() {
+        matrix_inverse
+    }
+    
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+
 }
 
 
@@ -12,4 +32,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    
+    result <- x$getinverse()
+    if(!is.null(result)) {
+        message("getting cached data")
+        return(result)
+    }
+    data <- x$get()
+    result <- solve(data)
+    x$setinverse(result)
+    result
+
 }
